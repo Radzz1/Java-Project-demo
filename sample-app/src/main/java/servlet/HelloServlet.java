e39095dd1afd3165e894604598c19de1c457b560
Radzz1 <rarun7857@gmail.com>
2026-04-01 19:24:05 +0530

    Update HelloServlet.java

--- sample-app/src/main/java/servlet/HelloServlet.java
+++ sample-app/src/main/java/servlet/HelloServlet.java
@@ -15,7 +15,7 @@ public class HelloServlet extends HttpServlet {
             throws ServletException, IOException {
         resp.setContentType("text/html");
         PrintWriter out = resp.getWriter();
-        out.println("<h2>Hello from Arun App - running on Java 17!</h2>");
+        out.println("<h2>Hello from UAT Branch - running on Java 17!</h2>");
     }
 }
 
